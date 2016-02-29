b8043303d5a1c289db5f6d8c4617ef5bcc3338bb
Simon Kurka <dev@simon.kurka.cc>
2016-02-29 15:34:55 +0100

    syntax error

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,7 +7,10 @@ class icvpn::config inherits icvpn {
     '/etc/tinc/icvpn/tinc.conf':
       ensure  => file,
       mode    => '0644',
-      content => epp('icvpn/tinc.conf.epp', { community => $community, node => $node }),
+      content => epp('icvpn/tinc.conf.epp', {
+        community => $community,
+        'node'    => $node,
+      }),
       notify  => Service['tinc'];
   } ->
   file {
@@ -25,7 +28,11 @@ class icvpn::config inherits icvpn {
     '/etc/bird/bird.conf.d/icvpn.conf':
       ensure  => file,
       mode    => '0644',
-      content => epp('icvpn/bird.epp', { allnets => $allnets, transfer_net => $transfer_net, local_as => $local_as }),
+      content => epp('icvpn/bird.epp', {
+        allnets      => $allnets,
+        transfer_net => $transfer_net,
+        local_as     => $local_as,
+      }),
       notify  => File['/etc/bird/bird.conf'];
   } ->
   file {
@@ -37,14 +44,20 @@ class icvpn::config inherits icvpn {
     '/etc/bird/bird6.conf.d/icvpn.conf':
       ensure  => file,
       mode    => '0644',
-      content => epp('icvpn/bird6.epp', { allnets6 => $allnets6, transfer_net6 => $transfer_net6, local_as => $local_as }),
+      content => epp('icvpn/bird6.epp', {
+        allnets6      => $allnets6,
+        transfer_net6 => $transfer_net6,
+        local_as      => $local_as,
+      }),
       notify  => File['/etc/bird/bird6.conf'];
   } ->
   file {
     '/opt/icvpn-scripts/icvpn-meta/.git/post-merge':
       ensure  => file,
       mode    => '0744',
-      content => epp('icvpn/icvpn-meta_post-merge.epp', { community => $community });
+      content => epp('icvpn/icvpn-meta_post-merge.epp', {
+        community => $community,
+      });
   }
 
   network::inet::static { 'icvpn':
